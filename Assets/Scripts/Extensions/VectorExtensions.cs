f0d0321ab7ebaf46a3acdfc02f357ec550d2d426
Michael Bartnett <michael@bartnett.com>
2014-01-25 18:28:32 -0500

    Make billboarding for sprites

---
+++ Assets/Scripts/Extensions/VectorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class VectorExtensions
+{
+    public static Vector2 ToXZ(this Vector3 vec3)
+    {
+        return new Vector2(vec3.x, vec3.z);
+    }
+}
---
+++ Assets/Scripts/OrientedBillboarder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrientedBillboarder : MonoBehaviour
+{
+    [SerializeField] private Sprite frontSprite;
+    [SerializeField] private Sprite backSprite;
+    [SerializeField] private SpriteRenderer childSprite;
+
+    //public bool FacingBackward { get; private set; }
+    
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        var camTransform = Camera.main.transform;
+        Debug.Log("ARGH", Camera.main);
+        float camDot = Vector2.Dot(this.transform.forward.ToXZ(), camTransform.position.ToXZ() - this.transform.position.ToXZ());
+        //if (camDot > 0 && FacingBackward) {
+        if (camDot > 0) {
+            childSprite.sprite = frontSprite;
+            //FacingBackward = false;
+        //} else if (!FacingBackward) {
+        } else {
+            //FacingBackward = true;
+            childSprite.sprite = backSprite;
+        }
+    }
+}

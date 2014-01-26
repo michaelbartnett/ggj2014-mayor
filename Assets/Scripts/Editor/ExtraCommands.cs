3d64ad9978d4a50f5529d669115ebdc664345e4f
Michael Bartnett <michael@bartnett.com>
2014-01-25 22:14:10 -0500

    All the characters

---
+++ Assets/Scripts/Editor/ExtraCommands.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public static class ExtraCommands
+{
+    [MenuItem("GameObject/Zero Parent Position Without Affecting Chldren")]
+    public static void ZeroParentPosition()
+    {
+        var parent = Selection.activeTransform;
+        var children = new Transform[parent.childCount];
+        for (int i = 0; i < children.Length; i++) {
+            children[i] = parent.GetChild(i);
+        }
+
+        var parentOffset = parent.localPosition;
+        for (int i = 0; i < children.Length; i++) {
+            children[i].localPosition += parentOffset;
+        }
+
+        parent.localPosition -= parentOffset;
+    }
+}
